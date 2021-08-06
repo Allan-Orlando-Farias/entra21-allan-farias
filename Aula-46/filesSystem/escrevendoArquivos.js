657b007326137f89e78645633bece1190a2f090c
Allan <allamfarias954@gmail.com>
2021-08-06 11:32:54 -0300

    Aula 46

---
+++ Aula-46/filesSystem/escrevendoArquivos.js
@@ -0,0 +1,22 @@
+const fsPromises = require("fs/promises");
+const path = require("path");
+const { EOL } = require("os");
+const os = require("os");
+
+setInterval(() => {
+    (async () => {
+        let freeMemoryInBytes = os.freemem(),
+            totalMemoryInBytes = os.totalmem(),
+            freeMemoryInMB = parseInt(freeMemoryInBytes / 1024 / 1024).toString(),
+            totalMemoryInMB = parseInt(totalMemoryInBytes / 1024 / 1024).toString(),
+            totalUsoMemoria = 
+        
+        try {
+            await fsPromises.appendFile(path.resolve(__dirname, "log.txt"), `Memória livre: ${freeMemoryInMB}MB\nMemória Total: ${totalMemoryInMB}MB\nUso da memória: ${totalUsoMemoria}%\n`);
+            
+            console.log("Novo conteúdo adicionado (Promise)");
+        } catch(err) {
+            console.log(err.message);
+        }
+        })();
+}, 1000);
