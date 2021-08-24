2d1472f16865299516a4845ef45f228fa1d52bd9
Allan <allamfarias954@gmail.com>
2021-08-24 11:53:46 -0300

    Testando APIs

---
+++ api_tutorial/index.js
@@ -0,0 +1,62 @@
+const { request, response } = require("express");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// Rotas da raíz
+app.get("/", (request, response) => {
+    response.send("Olá, Mundo!");
+});
+
+app.post("/", (request, response) => {
+    response.send("Método POST");
+});
+
+app.put("/", (request, response) => {
+    response.send("Método PUT");
+});
+
+app.delete("/", (request, response) => {
+    response.send("Método DELETE");
+});
+
+// Banco de dados
+const users = [
+    { id: 1, name: "Pedro", email: "pedro@email.com" },
+    { id: 2, name: "João", email: "joao@email.com" },
+    { id: 3, name: "Marcos", email: "marcos@email.com" },
+];
+
+
+// Rotas de usuário "/users"
+app.get("/users", (req, res) => {
+    res.json(users);
+});
+
+app.get("/users/:id", (req, res) => {
+    const userId = req.params.id;
+    
+     console.log(userId);
+    
+     const user = users.find(user => user.id == userId);
+    
+     if (!user) {
+    res.status(404).json({ message: "User not found!" });
+    }
+    
+     res.json(user);
+    });
+
+app.get("/users", (req, res) => {
+
+});
+
+app.get("/users", (req, res) => {
+
+});
+
+app.get("/users", (req, res) => {
+
+});
+
+app.listen(PORT, () => console.log("O servidor está rodando..."));
